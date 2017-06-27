b3371793a89915d332723accfb37bfd686f3f019
Abose U <bexis18@gmail.com>
2017-06-27 12:15:25 +0000

    add file upload functionality, add uploads route, and use cors and body-parser on theserver

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,14 @@
 const express = require("express"),
       router  = express.Router(),
-      multer  = require("multer");
+      multer  = require("multer"),
+      upload = multer({ dest: 'uploads/' });
       
-router.post("/",(request, response)=>{
-    response.send()
+router.post("/uploadFile", upload.single('myFile'),(request, response,next)=>{
+    //myFile is name attribute from input type=file from the html form
+    const fileSize = request.file.size;
+    if(fileSize){
+        response.send({file_size : fileSize});
+    }
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express    = require("express"),
-      router     = express.Router(),
+      router     = require("./routes/routes"),
+      app        = express(),
+      path       = require("path"),
       cors       = require("cors"),
       bodyParser = require("body-parser"),
       multer     = require("multer");
+      
+//app config
+app.use(cors());
+app.use(bodyParser.json());
+const port = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(router);
+
+
+app.listen(port, ()=>{
+    console.log("app is listening on port");
+})
