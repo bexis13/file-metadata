a9c961c5d71c250601155d558e7ff2a86b20e743
Abose U <bexis18@gmail.com>
2017-06-27 11:02:39 +0000

    add neccessary files for app structure and heroku deployment

---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+const express = require("express"),
+      router  = express.Router(),
+      multer  = require("multer");
+      
+router.post("/",(request, response)=>{
+    response.send()
+})
---
+++ server.js
@@ -0,0 +1,5 @@
+const express    = require("express"),
+      router     = express.Router(),
+      cors       = require("cors"),
+      bodyParser = require("body-parser"),
+      multer     = require("multer");
